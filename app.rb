20924adc1d4a313993da945175713f356255b06d
TarrinRos <tarindis@gmail.com>
2017-10-20 09:50:11 +0300

    Add cancel button and mock JS handler

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ get '/about' do
 end
 
 post '/cart' do
-  @orders_input = params[:orders]
+  @orders_input = params[:orders_input]
   @items = parse_orders_input @orders_input
 
   @items.each do |item|
@@ -34,7 +34,8 @@ post '/cart' do
 end
 
 post '/place_order' do
-  
+  @order = Order.create params[:order]
+  erb :order_placed
 end
 
 def parse_orders_input orders_input
--- public/app.js
+++ public/app.js
@@ -53,3 +53,9 @@ function cart_get_orders()
   }
   return orders;
 }
+
+function cancel_order()
+{
+  alert('aaa');
+  return false;
+}
